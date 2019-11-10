9fc868655f4314d087df6cb71d6bad2ac0e67987
Neel Kuila <neelk1123@gmail.com>
2019-11-09 19:36:37 -0500

    add user data

---
+++ user_data.sh
@@ -0,0 +1,11 @@
+yum install -y docker
+service docker start
+sudo docker run --detach \
+  --hostname gitlab.example.com \
+  --publish 443:443 --publish 80:80 \
+  --name gitlab \
+  --restart always \
+  --volume /srv/gitlab/config:/etc/gitlab \
+  --volume /srv/gitlab/logs:/var/log/gitlab \
+  --volume /srv/gitlab/data:/var/opt/gitlab \
+  gitlab/gitlab-ce:latest
